0e4231ac10b40dcbc7e2a4cb26868cff308328fa
Kai Prenger <kai.prenger@gmail.com>
2023-09-22 16:10:33 -0400

    stuttered print out of punct

--- punctuation.py
+++ punctuation.py
@@ -3,10 +3,16 @@ nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 import string
 import text 
+import time
 
 text = text.text 
 
 tokens = word_tokenize(text)
 punctuation_tokens = [token for token in tokens if token in string.punctuation]
 
-print(punctuation_tokens)
+# print(punctuation_tokens)
+
+for punct in punctuation_tokens:
+    for char in punct:
+        print(char, end='', flush=True)
+        time.sleep(0.03)  # delay for 0.5 seconds
