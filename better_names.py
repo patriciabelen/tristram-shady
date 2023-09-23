30face6ffacde24503d052de6182cc242276b558
Kai Prenger <kai.prenger@gmail.com>
2023-09-23 12:26:12 -0400

    much better names

--- better_names.py
+++ better_names.py
@@ -7,9 +7,10 @@ from nltk.tree import Tree
 
 shandy = text.shandy
 
-nltk_results = ne_chunk(pos_tag(word_tokenize(shandy)))
-for nltk_result in nltk_results:
-    if type(nltk_result) == Tree:
+results = ne_chunk(pos_tag(word_tokenize(shandy)))
+
+for result in results:
+    if type(result) == Tree:
         name = ''
         for nltk_result_leaf in nltk_result.leaves():
             name += nltk_result_leaf[0] + ' '
